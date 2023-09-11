193b1ab10acd3bb6ebcf1264f3054afd49397b9d
Sean <pca.sean.jeun@gmail.com>
2023-09-11 14:51:24 -0400

    weather.js done

--- practice-for-week-15-react-refactoring-class-components-long-practice-main/src/components/Weather.js
+++ practice-for-week-15-react-refactoring-class-components-long-practice-main/src/components/Weather.js
@@ -4,67 +4,60 @@ import { toQueryString } from '../utils';
 
 function Weather() {
     const [weather, setWeather] = useState(null)
-    
-    // componentDidMount() {
-    //   navigator.geolocation?.getCurrentPosition(
-    //     this.pollWeather,
-    //     (err) => console.log(err),
-    //     { timeout: 10000 }
-    //   );
-    // }
 
-    useEffect(()=> {  navigator.geolocation?.getCurrentPosition(
+    const pollWeather = async (location) => {
+    let url = 'http://api.openweathermap.org/data/2.5/weather?';
+
+    const apiKey = process.env.REACT_APP_WEATHER_API;
+
+    const params = {
+      lat: location.coords.latitude,
+      lon: location.coords.longitude,
+      appid: apiKey
+    };
+
+    url += toQueryString(params);
+
+    const res = await fetch(url);
+    if (res.ok) {
+      const result = await res.json();
+      setWeather(result);
+    }
+    else {
+      alert("Check Weather API key!")
+    }
+  }
+    useEffect(()=> { navigator.geolocation?.getCurrentPosition(
           pollWeather,
           (err) => console.log(err),
           { timeout: 10000 }
         );
+    }, [])
 
-   
 
-    pollWeather = async (location) => {
-      let url = 'http://api.openweathermap.org/data/2.5/weather?';
 
-      const apiKey = process.env.REACT_APP_WEATHER_API;
 
-      const params = {
-        lat: location.coords.latitude,
-        lon: location.coords.longitude,
-        appid: apiKey
-      };
-      
-      url += toQueryString(params);
+ /* --------------------------------------------------*/
 
-      const res = await fetch(url);
-      if (res.ok) {
-        const weather = await res.json();
-        this.setState({ weather });
-      }
-      else {
-        alert ("Check Weather API key!")
-      }
-    }
-  }, [])
+  let content = <div className='loading'>loading weather...</div>;
+  
+  if (weather) {
+    const temp = (weather.main.temp - 273.15) * 1.8 + 32;
+    content = (
+      <div>
+        <p>{weather.name}</p>
+        <p>{temp.toFixed(1)} degrees</p>
+      </div>
+    );
+  }
+  else {
+    content = (
+      <div>
+        Weather is currently unavailable. (Are Location Services enabled?) 
+      </div>
+    )
+  }
 
-  return (
-    const weather = this.state.weather;
-    let content = <div className='loading'>loading weather...</div>;
-    
-    if (weather) {
-      const temp = (weather.main.temp - 273.15) * 1.8 + 32;
-      content = (
-        <div>
-          <p>{weather.name}</p>
-          <p>{temp.toFixed(1)} degrees</p>
-        </div>
-      );
-    }
-    else {
-      content = (
-        <div>
-          Weather is currently unavailable. (Are Location Services enabled?) 
-        </div>
-      )
-    }
 
     return (
       <section className="weather-section">
@@ -74,7 +67,6 @@ function Weather() {
         </div>
       </section>
     );
-  )
 }
 
 export default Weather;
