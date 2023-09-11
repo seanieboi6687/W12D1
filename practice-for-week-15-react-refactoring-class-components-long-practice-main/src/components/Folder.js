4262b8dbe98b0219998df74f066db3392ad5ac7b
Sean <pca.sean.jeun@gmail.com>
2023-09-11 13:35:50 -0400

    switching

--- practice-for-week-15-react-refactoring-class-components-long-practice-main/src/components/Folder.js
+++ practice-for-week-15-react-refactoring-class-components-long-practice-main/src/components/Folder.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Headers = ({ titles, currentTab, selectTab }) => {
   const handleClick = (e) => {
@@ -28,19 +28,13 @@ const Headers = ({ titles, currentTab, selectTab }) => {
   );
 }
 
-class Folder extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentTab: 0
-    };
-  }
+function Folder(props) {
+  const [currentTab, setCurrentTab] = useState(0);
   
   selectTab = (num) => {
     this.setState({ currentTab: num });
   }
-  
-  render() {
+
     const folder = this.props.folders[this.state.currentTab];
     const titles = this.props.folders.map((folder) => folder.title);
     
@@ -59,7 +53,6 @@ class Folder extends React.Component {
         </div>
       </section>
     );
-  }
 }
 
 export default Folder;
